a93c0527be1334376293b8c0b7eea46f9a1a66cf
Gurvir Singh <gurvirsingh.baraich@gmail.com>
2024-06-06 11:02:02 +0530

    solved 4th problem

--- main.ml
+++ main.ml
@@ -16,5 +16,18 @@ let rec at (k: int) (xs: 'a list): 'a option =
   | [] -> None
   | head :: tail -> if k == 1 then Some head else at (k - 1) tail
 
+(* Problem 04 *)
+let rec length (xs: 'a list): int = 
+  match xs with
+  | [] -> 0
+  | _ :: tail -> 1 + length tail 
+
+let length_tail (xs: 'a list): int =
+  let rec length_tail' (acc: int) (xs: 'a list)  =
+    match xs with
+    | [] -> acc
+    | _ :: tail -> length_tail' (acc + 1) tail
+  in length_tail' 0 xs 
+
 let () =
   print_endline "Hello, World!"
